0320be68ebed647d1301f0c38ad37e633531d3d3
cyborgmg <cyborgmg@gmail.com>
2021-02-02 16:41:13 -0300

    Jenkinsfile

--- container.sh
+++ container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sshpass -p c6b94gmg ssh cyborg@192.168.0.8 /bin/bash -c "rm -rf /tmp/Dockerfile"
-# && rm -rf /tmp/tasks.war && rm -rf /tmp/tasks-backend.war
+sshpass -p c6b94gmg ssh cyborg@192.168.0.8 rm -rf /tmp/Dockerfile
+sshpass -p c6b94gmg ssh cyborg@192.168.0.8 rm -rf /tmp/tasks.war 
+sshpass -p c6b94gmg ssh cyborg@192.168.0.8 rm -rf /tmp/tasks-backend.war
 
 #sshpass -p c6b94gmg scp -oStrictHostKeyChecking=no Dockerfile cyborg@192.168.0.8:/tmp/
 
